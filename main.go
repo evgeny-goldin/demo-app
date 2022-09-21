acb4863aca08fe8584b4dbd50f24036511631f0b
Evgeny Goldin <evgenyg@gmail.com>
2022-09-21 11:16:46 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
-package main
+    package main
 
-import (
-	"fmt"
-	"os"
-	"time"
+    import (
+        "time"
 
-	"github.com/common-nighthawk/go-figure"
-)
+        "github.com/common-nighthawk/go-figure"
+    )
 
-func main() {
-	myFigure := figure.NewColorFigure("<USERNAME> is Awesome!!!", "larry3d", "yellow", true)
-
-	if secret := os.Getenv("SECRET"); secret != "" {
-		myFigure = figure.NewColorFigure(fmt.Sprintf("Secret value is: %s", secret), "larry3d", "yellow", true)
-	}
-	myFigure.Print()
-	time.Sleep(10 * time.Hour)
-}
+    func main() {
+        myFigure := figure.NewColorFigure("evgeny-goldin is Awesome!!!", "larry3d", "yellow", true)
+        myFigure.Print()
+        time.Sleep(10 * time.Hour)
+    }
